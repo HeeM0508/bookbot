4d345565eafdf35a83f2b1e08ff5f2422659d866
HeeM0508 <heemkill@gmail.com>
2025-11-17 04:28:19 +0300

    change_main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 def main(): 
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
-
+    count = words_count(text)
+    print (f"Found {count} total words")  
 
 def get_book_text(path):
-    with open(path) as f:
+    with open(path, encoding="utf-8") as f:
         return f.read()
- 
+
+def words_count(text):
+    num_words = len(text.split())
+    return num_words
 
 main()
-
-
